6edd69a7b1b986c98b6d00542acabfb1f05ee061
Fatima Modupe Bello <mailtomodupe@gmail.com>
2025-08-28 17:21:32 +0100

    updated index file

--- src/pages/Components/Landing/Product/index.tsx
+++ src/pages/Components/Landing/Product/index.tsx
@@ -57,7 +57,7 @@ const Product = () => {
                 <Navbar />
                 <Home />
                 <OurProduct />
-                  <OurProduct />
+              
        <FilterableDataTabs />
                 {/* <Features /> */}
                
